1932fefc5fcfacea3c31c328972a322b663071e2
RevTako <turo15@icloud.com>
2023-06-03 12:46:11 +0100

    removed service worker

--- app.js
+++ app.js
@@ -4,20 +4,7 @@ const app = express()
 //
 // serviceWorker.unregister();
 app.get('/', function (req, res) {
-  let today = new Date()
-  let time = today.getTime()
-  let ar = ['message1', 'message2']
   let msg = 'message 0'
-  console.log(msg)
-  msg = function () {
-    if (time%2===0) {
-      console.log(ar[1])
-      return(ar[1])
-    }else{
-      console.log(ar[0])
-      return(ar[0])
-    }
-  }
   console.log(msg)
   res.json({body : msg})
 })
