b858c5968628357c76346b0d7a4504faf9bc0f1b
RevTako <turo15@icloud.com>
2023-06-03 16:33:42 +0100

    none

--- app.js
+++ app.js
@@ -2,18 +2,9 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  const into = {user_input: '', SCARLET_output: '', msg: 'message 0'}
-  let today = new Date()
-  let time = today.getSeconds()
-  let ar = ['message1', 'message2']
-  const fun = function (time, ar) {
-    if (time%2===0) {
-      return(ar[1])
-    }else{
-      return(ar[0])
-    }
-  };
-  const outof = { ...into, msg: fun(time, ar) }
+  const into = {user_input: '', SCARLET_output: '', msg: 'no output to display'}
+  const fun = function () {};
+  const outof = { ...into, msg: 'no message to display' }
   res.json(outof)
 })
 
