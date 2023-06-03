45789fa917679af5b3adc5dc0a756be196e39b70
RevTako <turo15@icloud.com>
2023-06-03 16:53:13 +0100

    none

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  const into = {user_input: '', SCARLET_output: '', msg: 'no output to display'}
-  const fun = function () {};
-  const outof = { ...into, msg: 'no message to display' }
+app.get(
+    '/',
+    function (req, res) {
+  const outof = {msg: 'get requests to this page do nothing, use post from an api' }
   res.json(outof)
 })
+
+app.post(
+    '/',
+    function (req,res){
+      const send_to_scarlet = function (request) {
+        // send to SCARLET
+        return('SCARLET not connected')
+      }
+      const out = {
+        user_input: req.body,
+        SCARLET_output: send_to_scarlet(req.body),
+        msg:'no output to display'}
+      res.json(out)
+    }
+)
 
 
 app.listen(3000, function () {
