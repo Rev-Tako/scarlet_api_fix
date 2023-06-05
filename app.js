0d62867d6af41b4922a2f6eb09e4f7711eae0ab9
RevTako <turo15@icloud.com>
2023-06-05 17:34:35 +0100

    none

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get(
             })
                 .then(response => response.json())
                 .then(json =>
-                    res.json({API: 'ONLINE', SCARLET: 'OFFLINE'}))
+                    res.json({API: 'ONLINE', SCARLET: 'ONLINE'}))
         } catch {
         res.json({API: 'ONLINE', SCARLET: 'OFFLINE'})
         }
