c15633150f8f07769b7ea78e7c5962d878c19320
RevTako <turo15@icloud.com>
2023-06-05 17:38:17 +0100

    none

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get(
     function (req, res) {
 
         try {
-            fetch('/fetcher.js', {
+            fetch('fetcher.js', {
                 method: 'GET',
             })
                 .then(response => response.json())
@@ -21,7 +21,7 @@ app.post(
     '/',
     function (req,res){
       try {
-          fetch('/fetcher.js', {
+          fetch('fetcher.js', {
               method: 'POST',
               body: JSON.stringify(req.body)
           })
