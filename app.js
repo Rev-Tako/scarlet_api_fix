1749d9c8c07c1157e3debb0c146dce407a20794a
RevTako <turo15@icloud.com>
2023-06-20 16:38:28 +0100

    none

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 const express = require('express')
-const cors = require('cors')
 const app = express()
 const fetcher = require('./fetcher')
 const React = require('react')
 const axios = require("axios")
-
-app.use(cors())
-
 app.get(
     '/',
     async function (req, res) {
