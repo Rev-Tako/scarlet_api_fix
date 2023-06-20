36da41d9f073a8c9d25ad911663992b93c584800
RevTako <turo15@icloud.com>
2023-06-20 14:38:59 +0100

    none

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get(
     '/',
     async function (req, res) {
         try {
-            const fetched = Doget()
+            const fetched = fetcher.Doget()
             let returned = await fetched;
             res.json({
                 API: 'ONLINE',
@@ -36,7 +36,7 @@ app.post(
     '/',
     async function (req,res){
       try {
-          const fetched = Handler(req.body)
+          const fetched = fetcher.Handler(req.body)
           let returned = await fetched
           if (returned.scarlet && !(returned.scarlet.length === 0)) {
               const out = {
@@ -69,60 +69,6 @@ app.post(
 app.listen(3000, function () {
   console.log('SCARLET API listening on port 3000')
 })
-
-
-async function Doget() {
-    let URL = "http://tehr10.cis.strath.ac.uk:5055/"
-    try {
-        let response = await axios.get(URL);
-        let to_return = await response.data;
-        //console.log(to_return);
-        return {
-            statusCode: 200,
-            body: {
-                scarlet: to_return,
-                ermsg: 'http request succeeded'
-            }
-        }
-    } catch (err) {
-        console.log(err.message)
-        return{
-            statusCode: 200,
-            body: {
-                scarlet: 'OFFLINE',
-                ermsg: 'http request failed'
-            }
-        }
-    }
-}
-
-async function Handler(request) {
-    let URL = "http://tehr10.cis.strath.ac.uk:5055/"
-    let rasa_format = {
-        "sender": "user",  // sender ID of the user sending the message
-        "message": request
-    }
-    try {
-        let response = await axios.post(URL, rasa_format);
-        let to_return = await response.data;
-        return {
-            statusCode: 200,
-            body: {
-                scarlet: to_return.text,
-                ermsg: 'Success'
-            }
-        }
-    } catch (err) {
-        console.log(err) // output to netlify function log
-        return {
-            statusCode: 200,
-            body: {
-                scarlet: 'Response failed',
-                ermsg: 'Error: disconnect between API and SCARLET'
-            },
-        }
-    }
-}
 
 /*
 
--- fetcher.js
+++ fetcher.js
@@ -2,9 +2,58 @@ const axios = require("axios")
 const React = require('react')
 
 module.exports = {
+    async Doget() {
+        let URL = "http://tehr10.cis.strath.ac.uk:5055/"
+        try {
+            let response = await axios.get(URL);
+            let to_return = await response.data;
+            //console.log(to_return);
+            return {
+                statusCode: 200,
+                body: {
+                    scarlet: to_return,
+                    ermsg: 'http request succeeded'
+                }
+            }
+        } catch (err) {
+            console.log(err.message)
+            return{
+                statusCode: 200,
+                body: {
+                    scarlet: 'OFFLINE',
+                    ermsg: 'http request failed'
+                }
+            }
+        }
+    },
 
-
-
+    async Handler(request) {
+        let URL = "http://tehr10.cis.strath.ac.uk:5055/"
+        let rasa_format = {
+            "sender": "user",  // sender ID of the user sending the message
+            "message": request
+        }
+        try {
+            let response = await axios.post(URL, rasa_format);
+            let to_return = await response.data;
+            return {
+                statusCode: 200,
+                body: {
+                    scarlet: to_return.text,
+                    ermsg: 'Success'
+                }
+            }
+        } catch (err) {
+            console.log(err) // output to netlify function log
+            return {
+                statusCode: 200,
+                body: {
+                    scarlet: 'Response failed',
+                    ermsg: 'Error: disconnect between API and SCARLET'
+                },
+            }
+        }
+    }
 }
 
             //
