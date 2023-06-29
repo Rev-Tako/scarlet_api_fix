fb24fdc55334b70b034db2b1599a7dfbc171fff0
RevTako <turo15@icloud.com>
2023-06-29 16:00:06 +0100

    none

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.get(
             const fetched = fetcher.Doget()
             let returned = await fetched;
             res.json({
-                updated: 29062023_1556,
+                updated: 29062023_1559,
                 API: 'ONLINE',
                 SCARLET: returned.body.scarlet,
                 USER: 'This domain only accepts posts from netlify front end',
@@ -38,7 +38,7 @@ app.get(
             })
         } catch (err){
             res.json({
-                updated: 29062023_1556,
+                updated: 29062023_1559,
                 API: 'ONLINE',
                 SCARLET: 'CHECK FAILED',
                 USER: 'This domain only accepts posts from netlify front end',
